5d8ad94e647c6c0f7024ea934fbbebf709878651
lucataglia <luca.tagliabue@radicalbit.io>
2022-03-31 10:44:46 +0200

    create example
    add readme
    
    change text
    
    add button that invoke navigate from react-router-dom
    
    add explanation
    
    move explanation to the top
    
    refactor text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useNavigate } from 'react-router-dom';
 
+let count = 0
 function App() {
+  count = count + 1
+  console.debug('*** Re-render <App /> component');
+  
+  const navigate = useNavigate();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: 'flex', flexDirection: 'column', gap: '2rem'}}>
+      <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+        <strong>Intro</strong>
+        
+        The window.history.pushState method is useful when the URL must be updated to store the 
+        <br/>
+        the app state into the URL. That will allow the user to share the URL to someone else that
+        <br/>
+        will se exactly what the first user was seeing.
+      </div>
+
+      <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+        <strong>Demo</strong>
+
+        <div>
+          Execute the following line of code <strong>from the console of the Browser dev tools.</strong>
+          <br/>
+          The App component shouldn't be re-rendered. It will re-render if you click the button
+          <br/>
+          (please check the console.debug)
+        </div>
+
+        <pre>
+          {`window.history.pushState({}, \'\',  window.location.origin + window.location.pathname + \'?fakeParam=${count+1}\')`}
+        </pre>
+      </div>
+      
+      <div style={{display: 'flex', flexDirection: 'column', gap: '1rem'}}>
+        <button 
+          onClick={() => navigate({search: `fakeParam=${count+1}`}) }
+          style={{maxWidth: '350px'}}
+          >
+          Click here to invoke react-router-dom navigate
+          </button>
+        
+        App component re-render <strong>{count}</strong>
+      </div>
     </div>
   );
 }
